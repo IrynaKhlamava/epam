ba4ce50c4e19cf3a23558f37a05b5b5489401670
irynakhlamava <irynakhlamava@gmail.com>
2021-04-29 19:57:45 +0300

    2.2 practice, task 4

---
+++ src/com/company/practice2/arraysOfArrays/task4.java
@@ -0,0 +1,45 @@
+package com.company.practice2.arraysOfArrays;
+
+import java.util.Scanner;
+
+public class task4 {
+    /*
+    4. Сформировать квадратную матрицу порядка n по заданному образцу(n - четное):
+    1  2  3   ... n
+    n n-1 n-2 ... 1
+    1  2  3   ... n
+    n n-1 n-2 ... 1
+           ...
+    n n-1 n-2 ... 1
+     */
+    public static void main(String[] args) {
+        int elementMatrix = 1 ;
+        System.out.println("Enter matrix's size:");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (int) (Math.random() * 100 - 50);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    matrix[i][j] = elementMatrix++;
+                } else {
+                    matrix[i][j] = --elementMatrix;
+                }
+
+            }
+            System.out.println();
+        }
+        System.out.println("Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
