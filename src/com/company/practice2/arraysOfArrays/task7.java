0a08ba6a0705a2217ac36d062c56df55da01b740
irynakhlamava <irynakhlamava@gmail.com>
2021-04-30 19:49:20 +0300

    2.2 practice, task 7

---
+++ src/com/company/practice2/arraysOfArrays/task7.java
@@ -0,0 +1,30 @@
+package com.company.practice2.arraysOfArrays;
+
+import java.util.Scanner;
+
+public class task7 {
+    /*
+    7. Сформировать квадратную матрицу порядка N по правилу:
+    a[i][j] = sin ((i2 - j2)/n)
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter matrix's size:");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        double[][] matrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j]= Math.sin((Math.pow(i,2)-Math.pow(j,2))/n);
+            }
+            System.out.println();
+        }
+        System.out.println("Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
