de694a468e782ad0b486ff4f6b93900b47d69474
irynakhlamava <irynakhlamava@gmail.com>
2021-04-30 20:51:45 +0300

    2.2 practice, task 9

--- src/com/company/practice2/arraysOfArrays/task9.java
+++ src/com/company/practice2/arraysOfArrays/task9.java
@@ -13,12 +13,14 @@ public class task9 {
         int n = input.nextInt();
         int maxSum = 0, maxSumCol = 0, numCol = 0;
         int[][] matrix = new int[n][n];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = (int) (Math.random() * 100);
             }
         }
         System.out.println("Matrix from random numbers:");
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -26,6 +28,7 @@ public class task9 {
             System.out.println();
         }
         int j = 0;
+
         for (int i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 maxSumCol += matrix[j][i];
@@ -37,6 +40,7 @@ public class task9 {
             }
             maxSumCol = 0;
         }
+
         System.out.println("Max sum = "+ maxSum + " in column №" + numCol);
     }
 }
